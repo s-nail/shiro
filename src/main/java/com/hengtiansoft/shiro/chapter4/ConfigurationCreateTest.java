22587ba97cae4c30263e7b4f931281f24a0c2162
yuqiangjia <yuqiangjia@hengtiansoft.com>
2016-05-10 15:23:30 +0800

    INI配置

---
+++ src/main/java/com/hengtiansoft/shiro/chapter4/ConfigurationCreateTest.java
@@ -0,0 +1,66 @@
+package com.hengtiansoft.shiro.chapter4;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import junit.framework.Assert;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.pam.AtLeastOneSuccessfulStrategy;
+import org.apache.shiro.authc.pam.ModularRealmAuthenticator;
+import org.apache.shiro.authz.ModularRealmAuthorizer;
+import org.apache.shiro.authz.permission.WildcardPermissionResolver;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**INI配置分类:
+ * 
+
+[main]
+#提供了对根对象securityManager及其依赖的配置
+securityManager=org.apache.shiro.mgt.DefaultSecurityManager
+…………
+securityManager.realms=$jdbcRealm
+
+[users]
+#提供了对用户/密码及其角色的配置，用户名=密码，角色1，角色2
+username=password,role1,role2
+
+[roles]
+#提供了角色及权限之间关系的配置，角色=权限1，权限2
+role1=permission1,permission2
+
+[urls]
+#用于web，提供了对web url拦截相关的配置，url=拦截器[参数]，拦截器
+/index.html = anon
+/admin/** = authc, roles[admin], perms["permission1"]
+ */
+public class ConfigurationCreateTest {
+
+    @Test
+    public void test() {
+
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-config.ini");
+
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+
+        //将SecurityManager设置到SecurityUtils 方便全局使用
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        subject.login(token);
+
+        Assert.assertTrue(subject.isAuthenticated());
+
+
+
+    }
+}
